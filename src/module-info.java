0287b02f44b2b0910e84c75b02539f7ac1695649
uenerregly <reglycode@gmail.com>
2026-09-16 16:42:07 -0300

    Removing unnecessary fx files

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,9 @@
 module myFinancesApp {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens application to javafx.fxml;
+    exports application;
+
 }
